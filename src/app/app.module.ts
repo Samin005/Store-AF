9892917fb169828bfcf61d9ba91b9f2ce1dfdd96
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2019-05-01 02:08:17 +0600

    Particles and typing animation implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParticlesModule } from 'angular-particle';
+import { TypingAnimationDirective } from 'angular-typing-animation';
+import {Observable} from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypingAnimationDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
