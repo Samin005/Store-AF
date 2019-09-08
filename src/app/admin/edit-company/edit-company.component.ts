f29295c3385401d435a16730a6e9f3b9451fe48a
Samin005 <samin005.bracu@gmail.com>
2019-09-08 15:39:47 +0600

    Added bootstrap-select and implemented on edit-company in admin panel.

--- src/app/admin/edit-company/edit-company.component.ts
+++ src/app/admin/edit-company/edit-company.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {AdminService} from '../../service/admin.service';
 
+declare var $;
+
 @Component({
     selector: 'app-edit-company',
     templateUrl: './edit-company.component.html',
@@ -9,12 +11,21 @@ import {AdminService} from '../../service/admin.service';
 export class EditCompanyComponent implements OnInit {
 
     isLoggedIn = false;
+    selectedCompany: string;
+    companySelected = false;
 
     constructor(public adminService: AdminService) {
     }
 
     ngOnInit() {
         this.isLoggedIn = this.adminService.loggedIn;
+        $(document).ready(() => $('.selectpicker').selectpicker('refresh'));
+    }
+
+    onCompanySelect(value) {
+        this.selectedCompany = value;
+        this.companySelected = true;
+        console.log(this.selectedCompany);
     }
 
 }
