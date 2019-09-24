f595bf4990b03e2080a9afec2312851f2fc89098
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2019-09-24 23:05:12 +0600

    Added company name inside exported data from datatable buttons in back-office.

--- src/app/back-office/orders/orders.component.ts
+++ src/app/back-office/orders/orders.component.ts
@@ -1,51 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CompaniesService} from '../../service/companies.service';
+
 declare var $;
 
 @Component({
-  selector: 'app-orders',
-  templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.css']
+    selector: 'app-orders',
+    templateUrl: './orders.component.html',
+    styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+    constructor(public companiesService: CompaniesService) {
+    }
 
-  ngOnInit() {
-      $('#dataTable').DataTable({
-          dom: 'lfBtipr',
-          buttons: [
-              {
-                  extend: 'copyHtml5',
-                  text: '<i class="fas fa-copy"></i> Copy</button>',
-                  titleAttr: 'Copy',
-                  className: 'btn btn-primary-custom',
-              },
-              {
-                  extend: 'excelHtml5',
-                  text: '<i class="fas fa-file-excel"></i> Excel</button>',
-                  titleAttr: 'Excel',
-                  className: 'btn btn-primary-custom',
-              },
-              {
-                  extend: 'pdfHtml5',
-                  text: '<i class="fas fa-file-pdf"></i> PDF</button>',
-                  titleAttr: 'PDF',
-                  className: 'btn btn-primary-custom',
-              },
-              {
-                  extend: 'csvHtml5',
-                  text: '<i class="fas fa-file-csv"></i> CSV</button>',
-                  titleAttr: 'CSV',
-                  className: 'btn btn-primary-custom',
-              },
-              {
-                  extend: 'print',
-                  text: '<i class="fas fa-print"></i> Print</button>',
-                  titleAttr: 'Print',
-                  className: 'btn btn-primary-custom',
-              }
-          ]
-      });
-  }
+    ngOnInit() {
+        $('#dataTable').DataTable({
+            dom: 'lfBtipr',
+            buttons: [
+                {
+                    extend: 'copyHtml5',
+                    text: '<i class="fas fa-copy"></i> Copy</button>',
+                    titleAttr: 'Copy',
+                    className: 'btn btn-primary-custom',
+                    title: this.companiesService.company.name
+                },
+                {
+                    extend: 'excelHtml5',
+                    text: '<i class="fas fa-file-excel"></i> Excel</button>',
+                    titleAttr: 'Excel',
+                    className: 'btn btn-primary-custom',
+                    title: this.companiesService.company.name
+                },
+                {
+                    extend: 'pdfHtml5',
+                    text: '<i class="fas fa-file-pdf"></i> PDF</button>',
+                    titleAttr: 'PDF',
+                    className: 'btn btn-primary-custom',
+                    title: this.companiesService.company.name
+                },
+                {
+                    extend: 'csvHtml5',
+                    text: '<i class="fas fa-file-csv"></i> CSV</button>',
+                    titleAttr: 'CSV',
+                    className: 'btn btn-primary-custom',
+                    title: this.companiesService.company.name
+                },
+                {
+                    extend: 'print',
+                    text: '<i class="fas fa-print"></i> Print</button>',
+                    titleAttr: 'Print',
+                    className: 'btn btn-primary-custom',
+                    title: this.companiesService.company.name
+                }
+            ]
+        });
+    }
 
 }
