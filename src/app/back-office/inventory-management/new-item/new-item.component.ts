4a1419e93640e48096a0eeeee656e4b8f0e9e866
Samin005 <samin005.bracu@gmail.com>
2019-10-24 17:51:31 +0600

    Implemented multiple image files for new-item in back-office.

--- src/app/back-office/inventory-management/new-item/new-item.component.ts
+++ src/app/back-office/inventory-management/new-item/new-item.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Item} from '../../../model/item.model';
 import {ItemsService} from '../../../service/items.service';
 
+declare var $;
+
 @Component({
     selector: 'app-new-item',
     templateUrl: './new-item.component.html',
@@ -10,18 +12,25 @@ import {ItemsService} from '../../../service/items.service';
 export class NewItemComponent implements OnInit {
 
     item = new Item();
-    imgPaths: string;
 
     constructor(public itemsService: ItemsService) {
     }
 
     ngOnInit() {
+        // Add the following code if you want the name of the file appear on select
+        $('.custom-file-input').on('change', function() {
+            const fileName = $(this).val().split('\\').pop();
+            $(this).siblings('.custom-file-label').addClass('selected').html(fileName);
+        });
     }
 
     addItem() {
         this.item.id = this.item.name;
         this.item.imgPaths = [];
-        this.item.imgPaths.push(this.imgPaths);
+        const imgList = $('#imgPaths')[0].files;
+        for (const img of imgList) {
+            this.item.imgPaths.push(img.name);
+        }
         this.item.createDate = new Date();
         this.item.lastModified = new Date();
         this.itemsService.addItem(this.item);
