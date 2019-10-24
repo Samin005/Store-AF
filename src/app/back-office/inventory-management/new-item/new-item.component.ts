a35e8c26a4e1b27e2efb1c970c36b61699725b09
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2019-10-25 01:34:19 +0600

    Implemented add multiple image with thumbnail and delete selected image in new-item bo.

--- src/app/back-office/inventory-management/new-item/new-item.component.ts
+++ src/app/back-office/inventory-management/new-item/new-item.component.ts
@@ -12,25 +12,39 @@ declare var $;
 export class NewItemComponent implements OnInit {
 
     item = new Item();
+    imgUrls = [];
 
     constructor(public itemsService: ItemsService) {
     }
 
     ngOnInit() {
-        // Add the following code if you want the name of the file appear on select
-        $('.custom-file-input').on('change', function() {
-            const fileName = $(this).val().split('\\').pop();
-            $(this).siblings('.custom-file-label').addClass('selected').html(fileName);
-        });
+        this.item.imgPaths = [];
+    }
+
+    updateImgPaths(event) {
+        const files = event.target.files;
+        if (files) {
+            for (const file of files) {
+                const reader = new FileReader();
+                reader.onload = (e: any) => {
+                    this.imgUrls.push(e.target.result);
+                };
+                reader.readAsDataURL(file);
+                this.item.imgPaths.push(file.name);
+            }
+        }
+    }
+
+    removeFromImgPaths(url) {
+        const index = this.imgUrls.indexOf(url);
+        if (index !== -1) {
+            this.imgUrls.splice(index, 1);
+            this.item.imgPaths.splice(index, 1);
+        }
     }
 
     addItem() {
         this.item.id = this.item.name;
-        this.item.imgPaths = [];
-        const imgList = $('#imgPaths')[0].files;
-        for (const img of imgList) {
-            this.item.imgPaths.push(img.name);
-        }
         this.item.createDate = new Date();
         this.item.lastModified = new Date();
         this.itemsService.addItem(this.item);
