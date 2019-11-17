c9594d7f8ae88938428765bab8e4c494edb637f5
Samin005 <samin005.bracu@gmail.com>
2019-11-17 16:43:31 +0600

    Implemented auth service for OS.

--- src/app/online-store/header-os/header-os.component.ts
+++ src/app/online-store/header-os/header-os.component.ts
@@ -1,10 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {AngularFireAuth} from '@angular/fire/auth';
 import {Router} from '@angular/router';
 import {CompaniesService} from '../../service/companies.service';
 import {CartService} from '../../service/cart.service';
-import {auth} from 'firebase/app';
-import Swal from 'sweetalert2';
+import {AuthService} from '../../service/auth.service';
 
 @Component({
     selector: 'app-header-os',
@@ -14,7 +12,7 @@ import Swal from 'sweetalert2';
 export class HeaderOSComponent implements OnInit {
 
     constructor(private router: Router,
-                public afAuth: AngularFireAuth,
+                public authService: AuthService,
                 public companiesService: CompaniesService,
                 public cartService: CartService) {
     }
@@ -23,53 +21,11 @@ export class HeaderOSComponent implements OnInit {
     }
 
     signIn() {
-        Swal.fire({
-            title: 'Signing In...',
-            allowEscapeKey: false,
-            allowOutsideClick: false,
-            onBeforeOpen: () => {
-                Swal.showLoading();
-            }
-        }).finally();
-        this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
-            .then(response => {
-            Swal.fire({
-                type: 'success',
-                title: 'Sign In Successful!',
-                html: 'Welcome, <b>' + response.user.displayName + '</b>',
-                confirmButtonText: 'Great!',
-                timer: 3000
-            }).finally();
-        }).catch(reason => {
-            Swal.fire({
-                type: 'error',
-                title: 'Sign In Failed!',
-                text: reason
-            }).finally();
-        });
+        this.authService.signInOS();
     }
 
     signOut() {
-        Swal.fire({
-            title: 'Signing Out...',
-            onBeforeOpen: () => {
-                Swal.showLoading();
-            }
-        }).finally();
-        this.afAuth.auth.signOut()
-            .then(() => {
-                Swal.fire({
-                    type: 'success',
-                    title: 'Signed Out!',
-                    timer: 1500
-                }).finally();
-            }).catch(reason => {
-                Swal.fire({
-                    type: 'error',
-                    title: 'Sign Out Failed...',
-                    text: reason
-                }).finally();
-        });
+        this.authService.signOutOS();
     }
 
     viewCart() {
