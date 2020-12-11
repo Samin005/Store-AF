df93887444cd9cb52a4daff568ad42eebf7fbdee
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2020-12-12 01:05:09 +0600

    Added ts-particles to online-store.

--- src/app/common-components/ts-particles/ts-particles.component.ts
+++ src/app/common-components/ts-particles/ts-particles.component.ts
@@ -26,7 +26,7 @@ export class TsParticlesComponent implements OnInit {
             position: 'fixed',
             width: '100%',
             height: '100%',
-            'z-index': -1,
+            // 'z-index': -1,
             top: 0,
             left: 0,
             right: 0,
